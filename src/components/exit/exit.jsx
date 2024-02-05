562ac2c2a6dcfea840680836cd4edb20b1c2406f
Your Name <you@example.com>
2024-02-05 17:10:12 +0500

    1

---
+++ src/components/exit/exit.jsx
@@ -0,0 +1,15 @@
+import cn from 'classnames';
+import styles from './Button.module.css';
+import { ButtonProps } from './Button.props';
+
+function Button({ children, className, appearence = 'small', ...props }: ButtonProps) {
+    return (
+        <button className={cn(styles['button'], styles['accent'], className, {
+
+            [styles['small']]: appearence === 'small',
+            [styles['big']]: appearence === 'big'
+        })} {...props}>{children}</button>
+    );
+}
+
+export default Button;
